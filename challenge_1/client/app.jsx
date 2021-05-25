283513e76f6d724a0a59e30e5f20876d486ac573
jasonschreib <jwschreiber1@gmail.com>
2021-05-25 12:50:55 -0700

    Updating state with events on componentDidMount

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import axios
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    }
+  }
+
+  //when the component mounts, set the events array in state to be equal to the data in db.json by sending get request
+  componentDidMount() {
+    //send an axios get request
+    axios.get('/events')
+      .then((results) => {
+        console.log('RESULTS', results.data);
+        //then populate the state
+        this.setState({
+          events: results.data
+        });
+      })
+      .catch((err) => {
+        console.log('There was an error with componentDidMount get request');
+      });
+  }
 
 
+
+
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Historical Events Finder</h1>
+        </div>
+        <form>
+          <label>
+            Search by any Keyword:
+    <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <div>
+          Events:
+
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(< App />, document.getElementById('app'));
