acbe735b022c6a9c477e95fd0aaec2591be1da04
jasonschreib <jwschreiber1@gmail.com>
2021-05-26 12:38:34 -0700

    All events rendering on the page upon load

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReacliOM from 'react-dom';
 
 //import axios
 import axios from 'axios';
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [{ "date": "-300", "description": "Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.", "lang": "en", "category1": "By place", "category2": "Greece", "granularity": "year" }]
     }
   }
 
@@ -17,11 +17,11 @@ class App extends React.Component {
     //send an axios get request
     axios.get('/events')
       .then((results) => {
-        console.log('RESULTS', results.data);
-        //then populate the state
+        // then populate the state
         this.setState({
           events: results.data
         });
+        console.log('Events', this.state.events);
       })
       .catch((err) => {
         console.log('There was an error with componentDidMount get request');
@@ -48,7 +48,21 @@ class App extends React.Component {
 
         <div>
           Events:
-
+          {/* For each item in the state array object, list out its date, description, and language */}
+          {Object.entries(this.state.events).map((currEvent) => {
+            console.log('CURR event', currEvent[1]);
+            return (
+              <div>
+                <ul> date: {currEvent[1].date}
+                  <li>description: {currEvent[1].description}</li>
+                  <li>lang: {currEvent[1].lang}</li>
+                  <li>category1: {currEvent[1].category1}</li>
+                  <li>category2: {currEvent[1].category2}</li>
+                  <li>granularity: {currEvent[1].granularity}</li>
+                </ul>
+              </div>
+            )
+          })}
         </div>
 
       </div>
@@ -57,4 +71,4 @@ class App extends React.Component {
 }
 
 
-ReactDOM.render(< App />, document.getElementById('app'));
+ReacliOM.render(< App />, document.getElementById('app'));
