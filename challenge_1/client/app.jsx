bbd7059ddf9c8ac977394dc3ada64fdc20d43f49
jasonschreib <jwschreiber1@gmail.com>
2021-05-26 13:36:29 -0700

    form search functionality working

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -8,14 +8,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: [{ "date": "-300", "description": "Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.", "lang": "en", "category1": "By place", "category2": "Greece", "granularity": "year" }]
+      events: [{ "date": "-300", "description": "Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.", "lang": "en", "category1": "By place", "category2": "Greece", "granularity": "year" }],
+      currentSearch: ''
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    // searchKeyword = this.searchKeyword.bind(this);
   }
 
   //when the component mounts, set the events array in state to be equal to the data in db.json by sending get request
   componentDidMount() {
     //send an axios get request
-    axios.get('/events')
+    axios.get('/events?_limit=10')
       .then((results) => {
         // then populate the state
         this.setState({
@@ -28,8 +32,32 @@ class App extends React.Component {
       });
   }
 
+  // function that will automatically update the state as the user types
+  handleChange(event) {
+    this.setState({currentSearch: event.target.value});
+    console.log('Search', this.state.currentSearch);
+  }
 
 
+  //function when the submit button is clicked - will filter events based on searched keyword
+  // searchKeyword() {
+  //   //get the keyword searched
+  //   var keyword = this.state.currentSearch;
+  //   //send a get request with the keyword as the filer
+  //   axios.get(`/events/?=${keyword}`)
+  //     .then((results) => {
+  //       //TODO: if the results sent back are empty
+  //         //then...??
+  //       //set state to be equal to the results returned
+  //       this.setState({
+  //         events: results.data
+  //       });
+  //       console.log('Events', this.state.events);
+  //     })
+  //     .catch((err) => {
+  //       console.log('There was an error retrieving events with the searche keyword', err);
+  //     });
+  // }
 
 
   render() {
@@ -41,16 +69,17 @@ class App extends React.Component {
         <form>
           <label>
             Search by any Keyword:
-    <input type="text" name="name" />
+    <input type="text" name="keyword" value={this.state.currentSearch} onChange={this.handleChange}/>
           </label>
           <input type="submit" value="Submit" />
+          {/* onSubmit={this.searchKeyword} */}
         </form>
 
         <div>
           Events:
           {/* For each item in the state array object, list out its date, description, and language */}
           {Object.entries(this.state.events).map((currEvent) => {
-            console.log('CURR event', currEvent[1]);
+            // console.log('CURR event', currEvent[1]);
             return (
               <div>
                 <ul> date: {currEvent[1].date}
@@ -64,7 +93,6 @@ class App extends React.Component {
             )
           })}
         </div>
-
       </div>
     )
   }
