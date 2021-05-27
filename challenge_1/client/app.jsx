8ff9c8444566973a663f6ef6e875f26894f7530b
jasonschreib <jwschreiber1@gmail.com>
2021-05-27 13:51:04 -0700

    calculating number of pages correctly

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReacliOM from 'react-dom';
 
+//import react-paginate
+import ReactPaginate from 'react-paginate';
+
 //import axios
 import axios from 'axios';
 
@@ -9,21 +12,28 @@ class App extends React.Component {
     super(props);
     this.state = {
       events: [{ "date": "-300", "description": "Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.", "lang": "en", "category1": "By place", "category2": "Greece", "granularity": "year" }],
-      currentSearch: ''
+      currentSearch: '',
+      offset: 0,
+      eventsPerPage: 10,
+      currentPage: 0,
+      pageCount: 10
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.searchKeyword = this.searchKeyword.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
 
   //when the component mounts, set the events array in state to be equal to the data in db.json by sending get request
+  //TODO: SET THE PAGE COUNT TO CORRECT NUM OF PAGES WHEN COMPONENT MOUNTS
   componentDidMount() {
     //send an axios get request
-    axios.get('/events?_limit=10')
+    axios.get('/events?_page=1')
       .then((results) => {
         // then populate the state
         this.setState({
           events: results.data
+          //pageCount: ??
         });
         console.log('Events', this.state.events);
       })
@@ -46,21 +56,41 @@ class App extends React.Component {
     //get the keyword searched
     var keyword = this.state.currentSearch;
     console.log('keyword', keyword);
-    //send a get request with the keyword as the filer
+    //send a get request with the keyword as the filter
     axios.get(`/events/?q=${keyword}`)
       .then((results) => {
+        //calculate number of pages
+        var numPages = Math.ceil(results.data.length / this.state.eventsPerPage);
+        console.log('RESULTS', results)
         //TODO: if the results sent back are empty
         //then...??
         //set state to be equal to the results returned
         this.setState({
-          events: results.data
+          events: results.data,
+          pageCount: numPages
         });
-        console.log('Events', this.state.events);
+        console.log('numberPages', this.state.pageCount);
       })
       .catch((err) => {
-        console.log('There was an error retrieving events with the searche keyword', err);
+        console.log('There was an error retrieving events with the searched keyword', err);
       });
   }
+
+
+  //function to handle the page click
+  handlePageClick = (e) => {
+    console.log('PAGE CLICK', e);
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.eventsPerPage;
+
+    this.setState({
+        currentPage: selectedPage,
+        offset: offset
+    }, () => {
+    });
+    //invoke get request to retrieve data for the clicked page
+    console.log('PAGE CLICK STATE', this.state.currentPage, this.state.offset);
+};
 
 
   render() {
@@ -74,7 +104,7 @@ class App extends React.Component {
             Search by any Keyword:
     <input type="text" name="keyword" value={this.state.currentSearch} onChange={this.handleChange} />
           </label>
-          <button type="submit" value="Submit" onClick={this.searchKeyword}/>
+          <button type="submit" onClick={this.searchKeyword}> Submit </button>
         </form>
 
         <div>
@@ -94,6 +124,21 @@ class App extends React.Component {
               </div>
             )
           })}
+        </div>
+        <div>
+          <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    // breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    // containerClassName={"pagination"}
+                    // subContainerClassName={"pages pagination"}
+                    // activeClassName={"active"}
+                    />
         </div>
       </div>
     )
