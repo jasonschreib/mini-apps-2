3aeb229e7ec61ebd4cdf4c4e4d95a86ee6785804
jasonschreib <jwschreiber1@gmail.com>
2021-06-21 21:30:30 -0700

    Finished init and all setup, past webpack config errors

---
+++ challenge_3/client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Hey there in App component
+
+      </div>
+    )
+  }
+
+}
+
+export default App;
