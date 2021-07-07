aedbb992094832b86259aeb6d07c54c4480967ec
jasonschreib <jwschreiber1@gmail.com>
2021-07-06 17:18:29 -0700

    Implemented webpack and babel, working with api now

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    }
+  }
+
+  componentDidMount() {
+    //when the component mounts make a request to the coindesk api
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="https://www.coindesk.com/price/bitcoin">Powered by Coindesk</a>
+
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(< App/>, document.getElementById('app'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 7676
 
+app.use(express.static('./public'));
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
 
